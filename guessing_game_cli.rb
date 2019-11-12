8e650bafa64cf4612e02593afee6b0b98590a37b
Danielle Matton <danimatton@gmail.com>
2019-11-12 16:20:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  bye = "Goodbye!"
+  puts "Goodbye!"
   rand_num = rand(1..6)
   prompt_user = "Guess a number between 1 and 6"
   user_response = gets.chomp
