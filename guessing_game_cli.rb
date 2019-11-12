c57d05f6e5c3e85c033c51c1608d568db52a00f3
Danielle Matton <danimatton@gmail.com>
2019-11-12 16:24:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   puts "Goodbye!"
   puts = "Sorry! The computer guessed 6."
-  rand_num = rand(1..6)
+  rand_num = rand(6) + 1
   prompt_user = "Guess a number between 1 and 6"
   user_response = gets.chomp
 end
