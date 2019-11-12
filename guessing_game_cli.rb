8a2015d337cfe2cc1f46dceca07549d7f6c8a39d
Danielle Matton <danimatton@gmail.com>
2019-11-12 16:46:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   rand_num = rand(6) + 1
   prompt_user = "Guess a number between 1 and 6"
-  guess = gets.chomp
+  guess = gets.to_i
   if guess == rand_num
       puts "You guessed the correct number!"
   else 
